86d9f3cb19b387c3d78df5f4beafa32b0cc37e17
utsho <utsho111099@gmail.com>
2022-04-05 22:58:55 +0600

    not found added

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { EmojiSadIcon } from '@heroicons/react/solid'
-
+import './NotFound.css'
 const NotFound = () => {
     return (
-        <div className='relative notFound'>
-            <div>
-                <h2 className='text-center w-full'>
+        <div className='notFound'>
+            <div className='icon'>
+                {/* hello */}
+                <h2 className='text-2xl'>
                     <EmojiSadIcon className='w-24 h-24 text-center'></EmojiSadIcon>
+                    page not found
                 </h2>
             </div>
-        </div>
+        </div >
     );
 };
 
